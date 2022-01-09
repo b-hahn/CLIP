1b3f1bebc612d5471be6146e9cd7e74b71cda4c9
Benjamin Hahn <benjamin.hahn1@gmail.com>
2022-01-09 15:26:39 +0000

    WIP

--- simple_retraining.py
+++ simple_retraining.py
@@ -22,7 +22,7 @@ np.random.seed(0)
 
 SAVE_INTERVAL = 1
 BATCH_SIZE = 64
-NUM_EPOCHS = 10
+NUM_EPOCHS = 1000
 
 #BATCH_SIZE must larger than 1
 # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -46,6 +46,7 @@ else:
 
 train_dataloader = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder(
     "data/coco_crops_few_shot/train", transform=preprocess),
+                                                shuffle=True,
                                                batch_size=BATCH_SIZE)
 
 val_dataloader = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder(
@@ -58,8 +59,51 @@ optimizer = torch.optim.Adam(
     model.parameters(), lr=5e-5, betas=(0.9, 0.98), eps=1e-6,
     weight_decay=0.2)  #Params used from paper, the lr is smaller, more safe for fine tuning to new dataset
 
+# validation accuracy
+values_list, indices_list = [], []
+top5_results = []
+top1_results = []
+acc_top1_list = []
+acc_top5_list = []
+
+num_batches_val = len(val_dataloader.dataset)/BATCH_SIZE
+for i, batch in enumerate(tqdm(val_dataloader, total=num_batches_val)):
+    images, class_ids = batch
+    class_ids = class_ids.to(device)
+    # image_input = preprocess(image).unsqueeze(0).to(device)
+    image_input = images.to(device)
+    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in val_dataloader.dataset.classes]).to(device)
+
+    # Calculate features
+    with torch.no_grad():
+        image_features = model.encode_image(image_input)
+        text_features = model.encode_text(text_inputs)
+
+    # Pick the top 5 most similar labels for the image
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    # values, indices = similarity[0]#.topk(5)
+
+    acc_top1 = torchmetrics.functional.accuracy(similarity, class_ids)
+    acc_top5 = torchmetrics.functional.accuracy(similarity, class_ids, top_k=5)
+    # values_list.append(values)
+    # indices_list.append(indices)
+    acc_top1_list.append(acc_top1)
+    acc_top5_list.append(acc_top5)
+    # TODO: Could CLIP spit out a super similar word to the one I'm looking for? Then I need some NLP approach to decide whether result is sufficiently synonymous.
+    # for cid in class_ids:
+    #     top5_results.append(True if cid in indices else False)
+    #     top1_results.append(True if cid == indices[0] else False)
+    # compute mean top5 accuracy and top1 accuracy
+mean_top5_accuracy = torch.stack(acc_top5_list).mean().cpu().numpy()
+print(f"Mean Top 5 Accuracy: {mean_top5_accuracy*100}%.")
+mean_top1_accuracy = torch.stack(acc_top1_list).mean().cpu().numpy()
+print(f"Mean Top 1 Accuracy: {mean_top1_accuracy*100}%.")
+
 num_batches_train = len(train_dataloader.dataset)/BATCH_SIZE
 for epoch in range(NUM_EPOCHS):
+    epoch_train_loss = 0
     j = 0
     print(f"Epoch: {epoch}")
     for batch in tqdm(train_dataloader,total=num_batches_train):
@@ -81,6 +125,8 @@ for epoch in range(NUM_EPOCHS):
         ground_truth = torch.arange(logits_per_image.shape[0], dtype=torch.long, device=device)
 
         total_loss = (loss_img(logits_per_image, ground_truth) + loss_txt(logits_per_text, ground_truth)) / 2
+        # print(f"Loss: {total_loss}")
+        epoch_train_loss += total_loss
         total_loss.backward()
         if device == "cpu":
             optimizer.step()
@@ -107,10 +153,6 @@ for epoch in range(NUM_EPOCHS):
 
     num_batches_val = len(val_dataloader.dataset)/BATCH_SIZE
     for i, batch in enumerate(tqdm(val_dataloader, total=num_batches_val)):
-        if i == 0:
-            print(f"{batch[0].mean()}")
-        # if i > 10:
-        #     break
         images, class_ids = batch
         class_ids = class_ids.to(device)
         # image_input = preprocess(image).unsqueeze(0).to(device)
@@ -138,8 +180,13 @@ for epoch in range(NUM_EPOCHS):
         # for cid in class_ids:
         #     top5_results.append(True if cid in indices else False)
         #     top1_results.append(True if cid == indices[0] else False)
+
+    print(f"Epoch {epoch} train loss: {epoch_train_loss / num_batches_train}")
+
     # compute mean top5 accuracy and top1 accuracy
     mean_top5_accuracy = torch.stack(acc_top5_list).mean().cpu().numpy()
     print(f"Mean Top 5 Accuracy: {mean_top5_accuracy*100}%.")
     mean_top1_accuracy = torch.stack(acc_top1_list).mean().cpu().numpy()
     print(f"Mean Top 1 Accuracy: {mean_top1_accuracy*100}%.")
+
+
